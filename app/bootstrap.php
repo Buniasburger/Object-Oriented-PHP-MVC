371839897b8068a3eabc93fd4c5be50d2a2ea188
Piotr Opozda <popozda@protonmail.com>
2018-10-25 00:12:00 +0200

    Create autoloader for core libraries, move core instantiaton to main index file

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,11 +1,9 @@
 <?php
 
-// Load libraries
-require_once __DIR__ . '/libraries/Core.php';
-require_once __DIR__ . '/libraries/Controller.php';
-require_once __DIR__ . '/libraries/Database.php';
-
 // Load config
 require_once __DIR__ . '/config/config.php';
 
-$core = new Core;
+// Load Core Libraries
+spl_autoload_register(function($class) {
+    require_once __DIR__ . '/libraries/' . $class . '.php';
+});
