9b04f928c56866c4a4c60dddd7519c5752bad9af
Buniasburger <popozda@protonmail.com>
2018-10-29 05:44:41 +0000

    Add header to index page, add description and app version to about us page

--- app/config/config.php
+++ app/config/config.php
@@ -6,6 +6,8 @@ define('APPROOT', dirname(__FILE__ , 2));
 define('URLROOT', 'http://share.posts');
 // Site Name
 define('SITENAME', 'SharePosts');
+// App Version
+define('APPVERSION', '1.0.0');
 
 // Database Credentials
 const DB_HOST = '127.0.0.1';
--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -4,13 +4,19 @@ class Pages extends Controller
 {
     public function index(): void
     {
-        $data = ['title' => 'SharePosts'];
+        $data = [
+            'title' => 'SharePosts',
+            'description' => 'Simple social network buil on the TraversyMVC PHP framework'
+        ];
         $this->view('pages/index', $data);
     }
 
     public function about(): void
     {
-        $data = ['title' => 'About Us'];
+        $data = [
+            'title' => 'About Us',
+            'description' => 'App to share posts with other users'
+        ];
         $this->view('pages/about', $data);
     }
 }
