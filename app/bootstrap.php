e88c3bf0db8710d6d7b617c634605174bf5a20db
Piotr Opozda <popozda@protonmail.com>
2018-10-30 10:11:46 +0100

    Create redirect helper function and load it in bootstrap file

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,6 +3,9 @@
 // Load config
 require_once __DIR__ . '/config/config.php';
 
+// Load redirect function
+require_once __DIR__ . '/helpers/url_helper.php';
+
 // Load Core Libraries
 spl_autoload_register(function($class) {
     require_once __DIR__ . '/libraries/' . $class . '.php';
