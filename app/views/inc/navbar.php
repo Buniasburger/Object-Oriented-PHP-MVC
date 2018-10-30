adb2eff2f28dbb0eef7bd5b87aa1c017aaf6d3ff
Piotr Opozda <popozda@protonmail.com>
2018-10-30 22:48:11 +0100

    Finish the project :)

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -16,20 +16,22 @@
                 </li>
             </ul>
             <ul class="navbar-nav ml-auto">
-            <?php if(empty($_SESSION['user_id'])): ?>
+                <?php if (empty($_SESSION['user_id'])): ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo URLROOT ?>/users/register">Register</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo URLROOT ?>/users/login">Login</a>
+                    </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo URLROOT ?>/users/register">Register</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo URLROOT ?>/users/login">Login</a>
-                </li>
-
-            <?php else: ?>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo URLROOT ?>/users/logout">Logout</a>
-                </li>
-            <?php endif ?>
+                <?php else: ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Welcome <?php echo $_SESSION['user_name'] ?></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo URLROOT ?>/users/logout">Logout</a>
+                    </li>
+                <?php endif ?>
             </ul>
         </div>
     </div>
